08a6d1ce9381c80a15da10cb528f9986fdbc444f
GuzhouYiyun <gzyy_yzc@qq.com>
2026-08-11 23:57:20 +0800

    BD202406 无限房间

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -1,5 +1,29 @@
 #include "bits_stdc++.h"
+using namespace std;
+typedef long long unsigned int ll;
 int main(void) {
-	std::cout << "hello world" << std::endl;
+
+	ll K;
+	cin >> K;
+
+	vector<ll> num(1, K);
+	while (true) {
+		ll up = num.size();
+		if (num[up - 1] < up) {
+			break;
+		}
+		num.push_back(num[up - 1] - num[up - 1] % up);
+		num[up - 1] %= up;
+	}
+
+	for (ll i = 0; i < num.size(); ++i) {
+		if (num[i] == 0) {
+			continue;
+		}
+		else {
+			cout << i << ' ' << num[i] << endl;
+		}
+	}
+
 	return 0;
 }
