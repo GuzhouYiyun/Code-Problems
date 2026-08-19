264334b5a1c536415a2058f43a47c1549ae33ba9
GuzhouYiyun <gzyy_yzc@qq.com>
2026-08-19 21:39:26 +0800

    BD202404 110串 使用 dp 算法。首先题面给出限制条件：修改不超过 k 个；长度为 n。因此两维，k 和个数，每格代表当前状态下的方案数。但是思考后得知，对于每次新加的数，前两个会影响他的方案数因此需要再加二维表示到达当前时前一位和当前位的数字。 但是，由于四维，最差有 1e8 个格子，每个是 long long ，8字节，内存会超限，所以需要压缩。因为从状态转移的式子可以看出只需两维，因此使用奇偶压缩。 大循环取当前位，对于此时的，只需看是0是1，和各自翻不翻，对比 110 即可。

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -1,32 +1,94 @@
 ﻿#include "bits_stdc++.h"
 using namespace std;
 typedef long long ll;
+#define MOD 998244353
 int main(void) {
 
-	ll N, B;
-	cin >> N >> B;
-
-	vector<ll> P(N), S(N);
-	for (ll i = 0; i < N; ++i) {
-		cin >> P[i] >> S[i];
+	ll n, k;
+	cin >> n >> k;
+	string s;
+	cin >> s;
+	vector<ll> num(n + 1);
+	for (int i = 1; i <= n; ++i) {
+		num[i] = s[i - 1] - '0';
 	}
 
-	vector<vector<ll>> dp(N + 1, vector<ll>(2, 1e18));
-	dp[0][0] = 0;
-	for (ll i = 0; i < N; ++i) {
-		ll use = P[i] / 2 + S[i], unuse = P[i] + S[i];
-		for (ll j = N; j; --j) {
-			dp[j][0] = min(dp[j - 1][0] + unuse, dp[j][0]);
-			dp[j][1] = min(min(dp[j - 1][0] + use, dp[j - 1][1] + unuse), dp[j][1]);
+	if (n == 1) {
+		if (k == 0) {
+			cout << 1;
+		}
+		else {
+			cout << 2;
+		}
+		return 0;
+	}
+	else if (n == 2) {
+		if (k == 0) {
+			cout << 1;
+		}
+		else if (k == 1) {
+			cout << 3;
+		}
+		else {
+			cout << 4;
+		}
+		return 0;
+	}
+
+	vector<vector<vector<vector<ll>>>> dp(2, vector<vector<vector<ll>>>(k + 1, vector<vector<ll>>(2, vector<ll>(2, 0))));
+	//前几位 & 1，能改几个，末尾两位cd
+	dp[2 & 1][0][num[1]][num[2]] = 1;
+	if (k >= 1) {
+		dp[2 & 1][1][!num[1]][num[2]] = 1;
+		dp[2 & 1][1][num[1]][!num[2]] = 1;
+	}
+	if (k >= 2) {
+		dp[2 & 1][2][!num[1]][!num[2]] = 1;
+	}
+	for (ll i = 3; i <= n; ++i) {
+
+		for (ll j = 0; j < k + 1; ++j) {
+			for (ll a = 0; a < 2; ++a) {
+				for (ll b = 0; b < 2; ++b) {
+					dp[i & 1][j][a][b] = 0;
+				}
+			}
+		}
+
+		for (ll j = 0; j <= k; ++j) {
+			if (num[i] == 1) {
+				//不翻
+				dp[i & 1][j][0][1] = (dp[(i - 1) & 1][j][0][0] + dp[(i - 1) & 1][j][1][0]) % MOD;
+				dp[i & 1][j][1][1] = (dp[(i - 1) & 1][j][0][1] + dp[(i - 1) & 1][j][1][1]) % MOD;
+				//翻
+				if (j >= 1) {
+					dp[i & 1][j][0][0] = (dp[(i - 1) & 1][j - 1][0][0] + dp[(i - 1) & 1][j - 1][1][0]) % MOD;
+					dp[i & 1][j][1][0] = dp[(i - 1) & 1][j - 1][0][1] % MOD;
+				}
+			}
+			else {
+				//不翻
+				dp[i & 1][j][0][0] = (dp[(i - 1) & 1][j][0][0] + dp[(i - 1) & 1][j][1][0]) % MOD;
+				dp[i & 1][j][1][0] = dp[(i - 1) & 1][j][0][1] % MOD;
+				//翻
+				if (j >= 1) {
+					dp[i & 1][j][0][1] = (dp[(i - 1) & 1][j - 1][0][0] + dp[(i - 1) & 1][j - 1][1][0]) % MOD;
+					dp[i & 1][j][1][1] = (dp[(i - 1) & 1][j - 1][0][1] + dp[(i - 1) & 1][j - 1][1][1]) % MOD;
+				}
+			}
 		}
 	}
 
-	for (ll i = N; i >= 0; --i) {
-		if (dp[i][0] <= B or dp[i][1] <= B) {
-			cout << i;
-			break;
+	ll ans = 0;
+	for (int i = 0; i <= k; ++i) {
+		for (ll a = 0; a <= 1; ++a) {
+			for (ll b = 0; b <= 1; ++b) {
+				ans = (ans + dp[n & 1][i][a][b]) % MOD;
+			}
 		}
 	}
+
+	cout << ans;
 
 	return 0;
 }
