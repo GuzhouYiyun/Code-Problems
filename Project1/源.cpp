94c3f65cdc41dff96f77cbe0ec766849e05b13c3
GuzhouYiyun <gzyy_yzc@qq.com>
2026-08-20 15:50:54 +0800

    P1140 [ICPC 2001 Taejon R] 相似基因 使用dp，打表。两个串是主维，其他在子问题中都一样，所以不需要。 初始化需要整行，因为dp过程推不出，且不统一。

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -1,48 +1,51 @@
 ﻿#include "bits_stdc++.h"
 using namespace std;
 typedef long long ll;
-
 int main(void) {
 
-	ll N;
-	cin >> N;
+	vector<vector<ll>> same = {
+		{ 0, -3, -4, -2, -1},
+		{-3,  5, -1, -2, -1},
+		{-4, -1,  5, -3, -2},
+		{-2, -2, -3,  5, -2},
+		{-1, -1, -2, -2,  5} };
+	map<char, ll> f = { {'0', 0}, {'A', 1}, {'C', 2}, {'G', 3}, {'T', 4} };
 
-	if (N == 1) {
-		cout << 0;
-		return 0;
+	ll a;
+	cin >> a;
+	vector<ll> sa(a + 1, 0);
+	for (ll i = 1; i <= a; ++i) {
+		char c;
+		cin >> c;
+		sa[i] = f[c];
+	}
+	ll b;
+	cin >> b;
+	vector<char> sb(b + 1, 0);
+	for (ll i = 1; i <= b; ++i) {
+		char c;
+		cin >> c;
+		sb[i] = f[c];
 	}
 
-	vector<int> temp(N, 0);
-	temp[1] = 0;
-	vector<int> prime;
-	vector<bool> isprime(N, true);
-	isprime[1] = false;
-	for (int i = 2; i < N; ++i) {
-		if (isprime[i]) {
-			prime.push_back(i);
-			temp[i] = i - 1;
+	//a的，b的
+	vector<vector<ll>> dp(a + 1, vector<ll>(b + 1, -1e18));
+
+	dp[0][0] = 0;
+	for (ll i = 1; i <= a; ++i) {
+		dp[i][0] = same[sa[i]][0] + dp[i - 1][0];
+	}
+	for (ll i = 1; i <= b; ++i) {
+		dp[0][i] = same[0][sb[i]] + dp[0][i - 1];
+	}
+
+	for (ll i = 1; i <= a; ++i) {
+		for (ll j = 1; j <= b; ++j) {
+			dp[i][j] = max({ dp[i - 1][j] + same[sa[i]][0], dp[i][j - 1] + same[0][sb[j]], dp[i - 1][j - 1] + same[sa[i]][sb[j]] });
 		}
-		for (auto x : prime) {
-			if (x * i >= N) {
-				break;
-			}
-			isprime[x * i] = false;
-			if (i % x == 0) {
-				temp[x * i] = temp[i] * x;
-				break;
-			}
-			else {
-				temp[x * i] = temp[i] * (x - 1);
-			}
-		}
 	}
 
-	ll ans = 0;
-	for (ll i = 2; i < N; ++i) {
-		ans += temp[i];
-	}
-
-	cout << ans * 2 + 3;
+	cout << dp[a][b];
 
 	return 0;
 }
