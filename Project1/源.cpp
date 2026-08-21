8c210eb2563ab66acc36e75c3d9479383019ac81
GuzhouYiyun <gzyy_yzc@qq.com>
2026-08-21 10:52:30 +0800

    P5020 [NOIP 2018 提高组] 货币系统 去掉能组成的。从头开始，如果是必须的，由它往右推可替换的

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -3,29 +3,36 @@ using namespace std;
 typedef long long ll;
 int main(void) {
 
-	ll N;
-	cin >> N;
-	vector<vector<ll>> dp(N + 1, vector<ll>(N + 1, 1e18));
-	vector<ll> m(N + 1, 0);
-	for (ll i = 1; i <= N; ++i) {
-		cin >> m[i];
-		m[i] += m[i - 1];
-		dp[i][i] = 0;
-	}
+	ll T;
+	cin >> T;
 
-	for (ll len = 2; len <= N; ++len) {
-		for (ll i = 1; i <= N; ++i) {
-			ll j = i + len - 1;
-			if (j > N) {
-				continue;
-			}
-			for (ll k = i; k < j; ++k) {
-				dp[i][j] = min(dp[i][k] + dp[k + 1][j] + m[j] - m[i - 1], dp[i][j]);
+	while (T--) {
+		
+		ll n;
+		cin >> n;
+		vector<ll> a(n + 1);
+		for (ll i = 1; i <= n; ++i) {
+			cin >> a[i];
+		}
+		sort(a.begin() + 1, a.end());
+
+		//false -> 不可以凑
+		vector<bool> dp(25001);
+		dp[0] = true;
+		ll ans = 0;
+		for (ll i = 1; i <= n; ++i) {
+			ll v = a[i];
+			if (dp[v] == false) {
+				++ans;
+				for (ll x = v; x <= a.back(); ++x) {
+					dp[x] = dp[x] or dp[x - v];
+				}
 			}
 		}
-	}
 
-	cout << dp[1][N];
+		cout << ans << endl;
+
+	}
 
 	return 0;
 }
